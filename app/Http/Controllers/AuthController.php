72ee3a7ef2e88d152f1154727d350866cbed169d
Taka-ho <j1403239@gmail.com>
2023-10-08 21:26:41 +0900

    AuthControllerを修正

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -14,7 +14,7 @@ class AuthController extends Controller
         $key = Str::uuid()->toString();
         $usersKey = Str::uuid()->toString();
         Redis::set($key, $usersKey);
-        Redis::expire($key, 10);
+        Redis::expire($key, 7200);
         return redirect('http://localhost:3000/exam/'.$usersKey.'&key='.$key);
     }
 
